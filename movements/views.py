99e8b31fdc9dde01d70c54416856de70dade6a70
Venganzaalchocolate <73942230+Venganzaalchocolate@users.noreply.github.com>
2020-12-03 23:59:04 +0100

    creando csv

--- movements/views.py
+++ movements/views.py
@@ -1,7 +1,14 @@
 from movements import app
 from flask import render_template
+import csv
 
 @app.route('/')
 
-def lisaMovimientos():
-    return render_template('movementsList.html', miTexto="YA veremos")
+def listaIngresos():
+    fingresos = open("movements/data/ingresos.csv", 'r')
+    csvReader = csv.reader(fingresos, delimiter=',', quotechar='"')
+    ingresos = list(csvReader)
+    
+    print(ingresos)
+    
+    return render_template('movementsList.html', datos=ingresos)
