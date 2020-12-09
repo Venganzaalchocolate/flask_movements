bba20b987920897787181b6b5e58519594695a93
Venganzaalchocolate <73942230+Venganzaalchocolate@users.noreply.github.com>
2020-12-09 22:35:45 +0100

    creando formularios

--- movements/views.py
+++ movements/views.py
@@ -1,5 +1,5 @@
 from movements import app
-from flask import render_template
+from flask import render_template, request
 import csv
 
 @app.route('/')
@@ -9,6 +9,17 @@ def listaIngresos():
     csvReader = csv.reader(fingresos, delimiter=',', quotechar='"')
     ingresos = list(csvReader)
     
-    print(ingresos)
+    sumador=0
+    for ingreso in ingresos:
+        sumador+= float(ingreso[2])
     
-    return render_template('movementsList.html', datos=ingresos)
+    return render_template('movementsList.html', datos=ingresos, total=sumador)
+
+@app.route('/creaAlta', methods=['GET', 'POST'])
+
+def nuevoIngreso():
+    if request.method == 'GET':
+        print ('soy un get')
+    else:
+        print ('soy un post')
+    return render_template("creaAlta.html")
