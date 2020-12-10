06943ac589da365781996d46b67182d27bd552b6
Venganzaalchocolate <73942230+Venganzaalchocolate@users.noreply.github.com>
2020-12-11 00:29:50 +0100

    creando BBDD

--- movements/views.py
+++ movements/views.py
@@ -1,25 +1,37 @@
 from movements import app
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 import csv
+import sqlite3
 
 @app.route('/')
 
 def listaIngresos():
-    fingresos = open("movements/data/ingresos.csv", 'r')
-    csvReader = csv.reader(fingresos, delimiter=',', quotechar='"')
-    ingresos = list(csvReader)
+    conn = sqlite3.connect('movements/data/datos.db')
+    c = conn.cursor()
     
+    c.execute('SELECT fecha, concepto, cantidad FROM movimientos;')
+    ingresos=c.fetchall()
     sumador=0
     for ingreso in ingresos:
         sumador+= float(ingreso[2])
+    conn.close()
     
     return render_template('movementsList.html', datos=ingresos, total=sumador)
 
 @app.route('/creaAlta', methods=['GET', 'POST'])
 
 def nuevoIngreso():
-    if request.method == 'GET':
-        print ('soy un get')
-    else:
-        print ('soy un post')
+    if request.method == 'POST':
+        conn = sqlite3.connect('movements/data/datos.db')
+        c = conn.cursor()
+        
+        c.execute('INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?,?,?);' ,
+                  (float(request.form.get('cantidad')),
+                  request.form.get('concepto'),
+                  request.form.get('fecha'))
+        )
+        
+        conn.commit()
+        conn.close()
+        return redirect(url_for('listaIngresos'))
     return render_template("creaAlta.html")
